700aeb48d23fbc76a0e1fe30ff676367e59bb802
Rewlandt <101276693+Rewlandt@users.noreply.github.com>
2023-11-14 17:24:08 +0300

    Delete task2 directory

--- task2/index.php
+++
@@ -1,8 +0,0 @@
-<?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Задание2");
-?><?$APPLICATION->IncludeComponent(
-	"simplecomp.catalog",
-	"",
-Array()
-);?>Text here....<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
